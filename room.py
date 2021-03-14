5038c9fa1ffd324612dc9d59c53cf26f154c2454
satoshi-tanaka <st34.satoshi@gmail.com>
2021-03-15 01:44:32 +0900

    able to connect with a player

---
+++ room.py
@@ -0,0 +1,5 @@
+class Room:
+
+    def __init__(self, id):
+        self.id = id
+        self.players = {'row': None, 'column': None}
---
+++ server.py
@@ -0,0 +1,53 @@
+import asyncio
+import json
+import websockets
+from room import Room
+
+
+USERS = set()
+ROOMS = {}  # {room id: Room}
+
+
+def action_event():
+    # got an action
+    return json.dumps({"next_state": "state"})
+
+
+async def notify_action():
+    # TODO:
+    if USERS:
+        message = action_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def register(websocket):
+    # TODO:
+    print(f'add user {websocket}')
+    print(f'number of users is {len(USERS)}')
+    USERS.add(websocket)
+
+
+async def unregister(websocket):
+    print(f'add user {websocket}')
+    print(f'number of users is {len(USERS)}')
+    USERS.remove(websocket)
+
+
+async def server(websocket, path):
+    # register(websocket) sends user_event() to websocket
+    await register(websocket)
+    try:
+        await websocket.send(action_event())
+        async for message in websocket:  # receive message
+            data = json.loads(message)
+            if data[""] == "":
+                pass
+                # await fun()
+    finally:
+        await unregister(websocket)
+
+
+start_server = websockets.serve(server, "localhost", 6789)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
