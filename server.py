c3f4bf516a5a506d664daa6cbe297867108e7c08
satoshi-tanaka <st34.satoshi@gmail.com>
2021-03-16 15:13:35 +0900

    work using flask

--- server.py
+++ server.py
@@ -47,40 +47,41 @@ def send_error(websocket, message=""):
     websocket.send(error_response(message))
 
 
-# def play_action(websocket, data):
-#     if "room_id" not in data:
-#         logging.error(f"No room id in data when next action. {data}")
-#         send_error(websocket, "no room id")
-#         return
-#     room_id = data["room_id"]
-#     if "play_action" not in data:
-#         logging.error(f"No play action in data when next action. {data}")
-#         send_error(websocket, "no play_action")
-#         return
-#     action = data["play_action"]
-#
-#     if room_id not in ROOMS:
-#         send_error(websocket, "room id is wrong")
-#         return
-#     room = ROOMS[room_id]
-#     if websocket not in PLAYERS:
-#         send_error(websocket, "No player")
-#         logging.error("This player does not exist.")
-#         return
-#     player = PLAYERS[websocket]
-#
-#     room.next_action(player, action)
+def play_action(websocket, data):
+    if "room_id" not in data:
+        logging.error(f"No room id in data when next action. {data}")
+        send_error(websocket, "no room id")
+        return
+    room_id = data["room_id"]
+    if "play_action" not in data:
+        logging.error(f"No play action in data when next action. {data}")
+        send_error(websocket, "no play_action")
+        return
+    action = data["play_action"]
+
+    if room_id not in ROOMS:
+        send_error(websocket, "room id is wrong")
+        return
+    room = ROOMS[room_id]
+    if websocket not in PLAYERS:
+        send_error(websocket, "No player")
+        logging.error("This player does not exist.")
+        return
+    player = PLAYERS[websocket]
+
+    room.next_action(player, action)
 
 
-# def send_rooms_list(websocket=None):
-#     # Send the rooms list to all players
-#     # This is called when a room is created
-#     # If websocket is not None, send the information to the player
-#     message = room_list_response()
-#     if websocket is not None:
-#         asyncio.wait([websocket.send(message)])
-#     else:
-#         asyncio.wait([player.websocket.send(message) for player in PLAYERS])
+def send_rooms_list(websocket=None):
+    # Send the rooms list to all players
+    # This is called when a room is created
+    # If websocket is not None, send the information to the player
+    message = room_list_response()
+    if websocket is not None:
+        websocket.send(message)
+    else:
+        for player in PLAYERS:
+            player.websocket.send(message)
 
 
 def make_room(websocket, data):
@@ -126,49 +127,49 @@ def make_room(websocket, data):
     room.send_playing()
 
 
-# def join_room(websocket, data):
-#     # handle data
-#     if "player_type" not in data:
-#         logging.error(f"No player_type in data when joining room. {data}")
-#         send_error(websocket)
-#         return
-#     player_type = PlayerType(data["player_type"])
-#     if "room_id" not in data:
-#         logging.error(f"No room id in data when joining room. {data}")
-#         send_error(websocket)
-#         return
-#     room_id = data["room_id"]
-#     if "name" not in data:
-#         logging.error(f"No name in data when joining room. {data}")
-#         player_name = None
-#     else:
-#         player_name = data["name"]
-#     if websocket not in PLAYERS:
-#         logging.error(f"No player {websocket} when joining room. {data}")
-#         send_error(websocket)
-#         return
-#     player = PLAYERS[websocket]
-#
-#     # set name
-#     if player_name is not None:
-#         if player_name == "None":  # None is not allowed
-#             player_name = "None1"
-#         player.name = player_name
-#
-#     if room_id not in ROOMS:
-#         logging.error(f"No room id in ROOMS when joining room. {data}")
-#         send_error(websocket, "no room id in Rooms joining room")
-#         return
-#     room = ROOMS[room_id]
-#
-#     # add the player to the room
-#     ok = room.add_player(player, player_type)
-#     if not ok:
-#         send_error(websocket, "This player cannot join the room.")
-#         return
-#
-#     # Send the play information
-#     room.send_playing()
+def join_room(websocket, data):
+    # handle data
+    if "player_type" not in data:
+        logging.error(f"No player_type in data when joining room. {data}")
+        send_error(websocket)
+        return
+    player_type = PlayerType(data["player_type"])
+    if "room_id" not in data:
+        logging.error(f"No room id in data when joining room. {data}")
+        send_error(websocket)
+        return
+    room_id = data["room_id"]
+    if "name" not in data:
+        logging.error(f"No name in data when joining room. {data}")
+        player_name = None
+    else:
+        player_name = data["name"]
+    if websocket not in PLAYERS:
+        logging.error(f"No player {websocket} when joining room. {data}")
+        send_error(websocket)
+        return
+    player = PLAYERS[websocket]
+
+    # set name
+    if player_name is not None:
+        if player_name == "None":  # None is not allowed
+            player_name = "None1"
+        player.name = player_name
+
+    if room_id not in ROOMS:
+        logging.error(f"No room id in ROOMS when joining room. {data}")
+        send_error(websocket, "no room id in Rooms joining room")
+        return
+    room = ROOMS[room_id]
+
+    # add the player to the room
+    ok = room.add_player(player, player_type)
+    if not ok:
+        send_error(websocket, "This player cannot join the room.")
+        return
+
+    # Send the play information
+    room.send_playing()
 
 
 def register(websocket):
@@ -188,7 +189,7 @@ def unregister(websocket):
 def server(websocket):
     # register(websocket) sends user_event() to websocket
     register(websocket)
-    # send_rooms_list(websocket)  # send the information of rooms
+    send_rooms_list(websocket)  # send the information of rooms
     while not websocket.closed:
         message = websocket.receive()
         if message is None:  # message is "None" if the client has closed.
@@ -201,10 +202,10 @@ def server(websocket):
             continue
         if data["action"] == "make-room":
             make_room(websocket, data)
-        # elif data["action"] == "join-room":
-        #     join_room(websocket, data)
-        # elif data["action"] == "play-action":
-        #     play_action(websocket, data)
+        elif data["action"] == "join-room":
+            join_room(websocket, data)
+        elif data["action"] == "play-action":
+            play_action(websocket, data)
         else:
             logging.error(f"Unexpected action request. {data}")
     unregister(websocket)
